ffea5196581823ed46859eaaf6d7891195e41423
harshicreative <hsicreative2000@gmail.com>
2021-08-10 12:38:50 +0530

    Added music apps

---
+++ music/app/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+<nav class="navbar navbar-dark bg-dark">
+  <div src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-FkEQ6JJzjw7x-amoesPeO4LiwswDvy7mlA&usqp=CAU" class="container-fluid">
+    <a  class="navbar-brand"  href="#">
+      <img style="border:radius" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-FkEQ6JJzjw7x-amoesPeO4LiwswDvy7mlA&usqp=CAU" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      Boom
+    </a>
+  </div>
+</nav>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
